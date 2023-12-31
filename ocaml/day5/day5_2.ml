41f4a1e235f3675ce128cdb8cf51e88987cb66d9
Arjun Chib <arjunpatrickchib@gmail.com>
2023-12-31 16:12:05 -0500

    Fix day 5 part 2

--- ocaml/day5/day5_2.ml
+++ ocaml/day5/day5_2.ml
@@ -1,80 +1,72 @@
 let file = "input.txt"
 
-type range = { dest_start : int; src_start : int; length : int }
+type mapping = { dest : int; src : int; len : int }
 
-let make_seq (start, length) = Seq.init length (fun x -> start + x)
+let rec group2 lst =
+  match lst with
+  | first :: second :: rest -> (first, second) :: group2 rest
+  | _ :: [] -> raise Exit
+  | [] -> []
 
-let rec partition_alternating l =
-  match l with
-  | fst :: snd :: l -> (fst, snd) :: partition_alternating l
-  | _ -> []
+let find_min lst =
+  lst |> List.fold_left (fun a b -> if a < b then a else b) max_int
 
-let parse_seeds seed_str =
-  match seed_str |> String.split_on_char ':' with
-  | _ :: seeds :: _ ->
-      seeds |> String.trim |> String.split_on_char ' ' |> List.map int_of_string
-      |> partition_alternating |> List.map make_seq
-      |> List.fold_left (fun accu s -> Seq.append accu s) Seq.empty
+let parse_seeds str =
+  str |> String.split_on_char ':' |> List.tl |> List.hd |> String.trim
+  |> String.split_on_char ' ' |> List.map int_of_string |> group2
+  |> List.map (fun (s, len) -> (s, s + len))
+
+let parse_mapping str =
+  match str |> String.split_on_char ' ' |> List.map int_of_string with
+  | [ first; second; third ] -> { dest = first; src = second; len = third }
   | _ -> raise Exit
 
-let parse_map map_str =
-  match
-    map_str |> String.trim |> String.split_on_char ' ' |> List.map int_of_string
-  with
-  | dest :: src :: len :: _ ->
-      { dest_start = dest; src_start = src; length = len }
+let parse_maps lst =
+  lst
+  |> List.map (fun str ->
+         str |> String.split_on_char ':' |> List.tl |> List.hd |> String.trim
+         |> String.split_on_char '\n' |> List.map parse_mapping)
+
+let parse_input str =
+  match str |> Str.split (Str.regexp "\n\n") with
+  | first :: rest -> (parse_seeds first, parse_maps rest)
   | _ -> raise Exit
 
-let parse_maps map_str =
-  match map_str |> String.split_on_char ':' with
-  | _ :: maps :: _ ->
-      maps |> String.trim |> String.split_on_char '\n' |> List.map parse_map
-  | _ -> raise Exit
+let lookup m input = input - m.src + m.dest
 
-let join a b = a ^ "\n" ^ b
+let overlap (s1, e1) (s2, e2) =
+  let s = if s1 < s2 then s2 else s1 in
+  let e = if e1 < e2 then e1 else e2 in
+  if e > s then Some (s, e) else None
 
-let parse_input lines =
-  match
-    lines
-    |> List.map (fun line -> match line with "" -> "," | _ -> line)
-    |> List.fold_left join "" |> String.split_on_char ','
-  with
-  | seed_str :: map_str -> (parse_seeds seed_str, map_str |> List.map parse_maps)
-  | _ -> raise Exit
+let rec traverse_mappings mappings (s1, e1) =
+  if s1 < e1 then
+    match mappings with
+    | m :: rest -> (
+        match overlap (s1, e1) (m.src, m.src + m.len) with
+        | Some (s2, e2) ->
+            (lookup m s2, lookup m e2)
+            :: (traverse_mappings rest (s1, s2)
+               @ traverse_mappings rest (e2, e1))
+        | None -> traverse_mappings rest (s1, e1))
+    | [] -> [ (s1, e1) ]
+  else []
 
-let memo_rec f =
-  let h = Hashtbl.create 16 in
-  let rec g x =
-    try Hashtbl.find h x
-    with Not_found ->
-      let y = f g x in
-      Hashtbl.add h x y;
-      y
-  in
-  g
+let rec traverse_maps maps input =
+  match maps with
+  | mp :: rest ->
+      traverse_mappings mp input
+      |> List.map (fun input' -> traverse_maps rest input')
+      |> find_min
+  | [] -> fst input
 
 let () =
   let ic = open_in file in
   try
-    let seeds, maps = In_channel.input_lines ic |> parse_input in
-    let memo_traverse =
-      let traverse_map self (input, i) =
-        match List.nth_opt maps i with
-        | None -> input
-        | Some map -> (
-            match
-              map
-              |> List.find_opt (fun r ->
-                     input >= r.src_start && input < r.src_start + r.length)
-            with
-            | Some r -> self (r.dest_start + (input - r.src_start), i + 1)
-            | None -> self (input, i + 1))
-      in
-      memo_rec traverse_map
-    in
+    let seeds, maps = In_channel.input_all ic |> parse_input in
     seeds
-    |> Seq.map (fun seed -> memo_traverse (seed, 0))
-    |> Seq.fold_left min max_int |> print_int;
+    |> List.map (fun seed -> traverse_maps maps seed)
+    |> find_min |> print_int;
     print_newline ();
     close_in ic
   with e ->
